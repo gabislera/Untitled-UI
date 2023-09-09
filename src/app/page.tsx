eb01885fc58caaca0c82b0778987a5cfc678841e
Gabriela Carniel <gabiicarniel@gmail.com>
2023-09-09 08:46:19 -0300

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,186 @@
+import { SettingsTabs } from '@/components/SettingsTabs'
+import * as Input from '@/components/Input'
+import * as FileInput from '@/components/Form/FileInput'
+import { Mail, UploadCloud, User } from 'lucide-react'
+
 export default function Home() {
   return (
-    <div className="">teste</div>
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+
+      <div className="mt-6 flex flex-col ">
+        <div className="flex items-center justify-between border-b border-zinc-200 pb-5 ">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-700 ">
+              Update your photo and personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50
+              "
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+              form="settings"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+
+        <form
+          action=""
+          id="settings"
+          className="mt-6 flex w-full flex-col gap-5 divide-y divide-zinc-200"
+        >
+          <div className="grid-cols-form grid gap-3">
+            <label
+              htmlFor="firstName"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Name
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <Input.Root>
+                <Input.Control id="firstName" defaultValue="Gabi" />
+              </Input.Root>
+
+              <Input.Root>
+                <Input.Control defaultValue="Carniel" />
+              </Input.Root>
+            </div>
+            <div />
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Email address
+            </label>
+            <Input.Root>
+              <Input.Prefix>
+                <Mail className="h-5 w-5 text-zinc-500" />
+              </Input.Prefix>
+              <Input.Control
+                type="email"
+                id="email"
+                defaultValue="gabiicarniel@gmail.com"
+              />
+            </Input.Root>
+
+            <div />
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label
+              htmlFor="photo"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Your photo
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                This will be displayed on your profile
+              </span>
+            </label>
+            <div >
+              <FileInput.Root className='flex items-start gap-5'>
+                <FileInput.ImagePreview />
+                <FileInput.Trigger />
+                <FileInput.Control />
+              </FileInput.Root>
+
+            </div>
+            <div />
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label htmlFor="role" className="text-sm font-medium text-zinc-700">
+              Role
+            </label>
+            <Input.Root>
+              <Input.Control type="text" id="role" defaultValue="Admin" />
+            </Input.Root>
+            <div />
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label
+              htmlFor="country"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Country
+            </label>
+            <div></div>
+            <div />
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label
+              htmlFor="timezone"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Timezone
+            </label>
+            <div></div>
+            <div />
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label htmlFor="bio" className="text-sm font-medium text-zinc-700">
+              Bio
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                Write a short introduction
+              </span>
+            </label>
+            <div></div>
+            <div />
+          </div>
+
+          <div className="grid-cols-form grid gap-3 pt-5">
+            <label
+              htmlFor="projects"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Portfolio projects
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500">
+                Share a few snippets of your work
+              </span>
+            </label>
+            <div>
+              <FileInput.Root >
+                <FileInput.Trigger />
+                <FileInput.Control multiple />
+              </FileInput.Root>
+            </div>
+            <div />
+          </div>
+
+          <div className="flex items-center justify-end gap-2 pt-5">
+            <button
+              type="button"
+              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50
+              "
+            >
+              Cancel
+            </button>
+            <button
+              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+              form="settings"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,29 +1,39 @@
-import { BarChart, CheckSquare, Cog, Flag, Home, LifeBuoyIcon, Search, SquareStack, Users } from "lucide-react";
-import { Logo } from "./Logo";
-import { NavItem } from "./NavItem";
-import { UsedSpaceWidget } from "./UsedSpaceWidget";
-import { Profile } from "./Profile";
-import { InputControl, InputPrefix, InputRoot } from "../Input";
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoyIcon,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+import { Logo } from './Logo'
+import { NavItem } from './NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
+import { Profile } from './Profile'
+import * as Input from '../../components/Input'
 
 export function Sidebar() {
   return (
-    <aside className='border-r border-zinc-200 px-5 py-8 flex flex-col gap-6'>
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <InputRoot>
-        <InputPrefix>
-          <Search className="h-5 mx-1 w-5 text-zinc-500" />
-        </InputPrefix>
-        <InputControl placeholder="Search" />
-      </InputRoot>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="mx-1 h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
       <nav className="space-y-0.5">
-        <NavItem title='Home' icon={Home} />
-        <NavItem title='DashBoard' icon={BarChart} />
-        <NavItem title='Projects' icon={SquareStack} />
-        <NavItem title='Tasks' icon={CheckSquare} />
-        <NavItem title='Reporting' icon={Flag} />
-        <NavItem title='Users' icon={Users} />
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="DashBoard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
       </nav>
 
       <div className="mt-auto flex flex-col gap-6">
